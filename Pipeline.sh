39bdb0dbf55782a54b7d2d1edcda47f017829aa1
Bram Ratz <bratz@uoguelph.ca>
2018-11-09 11:36:55 -0500

    Add files via upload
    Some ideas about how to start the pipeline. Starts by asking the user for fastq files then analyzes the file(s) with FastQC to determine the quality of the reads. Not sure how to properly load the all the modules that a user would actually need, so that part needs work
    possible future directions - based on the quality of the reads could either send the files to a program like TRIMMOMATIC to clean up the file (if the sequences are of high enough quality would simply skip this step - would ideally use something like a while statement to do this). After this sequence could be aligned, converted to a SAM file, converted to a BAM file, the use platypus for variant calling.

---
+++ Pipeline.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo =====================================
+#Packages to import
+echo =====================================
+
+#load modules that we are going to use in this pipeline
+
+module load fastqc
+
+module load sabre
+
+module load Platypus
+
+#loading required fastqc files
+#get users name
+echo 'What would you like to be called?'
+read usrname
+
+#need user to tell us where the fastq files are and provide the path to them
+echo '$usrname what is the path to your fastq file'
+read FASTQ
+  DATA=$FASTQ
+echo
+
+#need path to fastqc
+echo '$usrname what is the path to fastqc'
+read fastqc
+  TOOL_FASTQC=$fastqc
+echo
+
+#create directory for results of analysis
+mkdir -pv fastqc_results
+
+#copy fastq data there
+cp $DATA /fastqc_results
+
+#change the path
+cd fastqc_results
+
+#run fastqc on fastq file to check the quiality of the reads in the file and to assess whether or not data trimming needs to be done
+./$TOOL_FASTQC $DATA > Fastqc_results.txt
