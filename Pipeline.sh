06365546516e346e3f65212870444218fcc84b50
ilee21 <43783551+ilee21@users.noreply.github.com>
2018-11-09 12:14:18 -0500

    Update Pipeline.sh

--- Pipeline.sh
+++ Pipeline.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-
+==============================
+Assignemnt information
+==============================
+"""
+The goal of this assignment is to develop a pipeline to ______
+Completed by Bram Ratz, Gurkamal Deol, Shalini Suraj, and Ian Lee
+"""
 echo =====================================
 #Packages to import
 echo =====================================
 
-#load modules that we are going to use in this pipeline
+#Load modules that we are going to use in this pipeline
 
 module load fastqc
 
@@ -12,35 +18,31 @@ module load sabre
 
 module load Platypus
 
-echo =====================================
-#asking for user imput
-echo =====================================
-
-#loading required fastqc files
-#get users name
+#Loading required fastqc files
+#Get users name
 echo 'What would you like to be called?'
 read usrname
 
-#need user to tell us where the fastq files are and provide the path to them
+#Need user to tell us where the fastq files are and provide the path to them
 echo '$usrname what is the path to your fastq file'
 read FASTQ
   DATA=$FASTQ
 echo
 
-#need path to fastqc
+#Need path to fastqc
 echo '$usrname what is the path to fastqc'
 read fastqc
   TOOL_FASTQC=$fastqc
 echo
 
-#create directory for results of analysis
+#Create directory for results of analysis
 mkdir -pv fastqc_results
 
-#copy fastq data there
+#Copy fastq data there
 cp $DATA /fastqc_results
 
-#change the path
+#Change the path
 cd fastqc_results
 
-#run fastqc on fastq file to check the quiality of the reads in the file and to assess whether or not data trimming needs to be done
+#Run fastqc on fastq file to check the quiality of the reads in the file and to assess whether or not data trimming needs to be done
 ./$TOOL_FASTQC $DATA > Fastqc_results.txt
